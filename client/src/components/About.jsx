4c40a34fe837349be6e83bab317ee6df7ebc838e
Ravi <ravisingh007ravi@gmail.com>
2023-07-16 11:06:14 +0530

    create 2 page about and contactus

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,11 +1,118 @@
-import React from 'react'
+import { Box, Typography, Grid, styled, Button } from '@mui/material';
+import { Facebook, Instagram, GitHub, LinkedIn, CoPresentSharp } from '@mui/icons-material';
+import Ravi from '../images/ravi.jpg';
+import { Link } from 'react-router-dom';
+
+const ContainerMain = styled(Grid)`
+    margin: 100px 100px 0 0;
+`;
+
+const Images = styled('img')({
+  textAlign:'center',
+  marginTop:'50px',
+  borderRadius: '20px',
+  marginLeft: '50px',
+  height: '550px',
+    width: '500px'
+});
+
+const Header = styled(Typography)`
+    font-size: 55px;
+    font-weight: 600;
+    text-align: center;
+`;
+
+const SubHeader = styled(Typography)`
+    text-align: center;
+    color: #878787;
+    font-size: 35px;
+    margin-top: 20px;
+`;
+
+const BioData = styled(Typography)`
+    text-align: center;
+    font-size: 25px;
+    margin-top: 50px;
+`;
+
+const HomeButton = styled(Link)({
+  marginLeft: '360px'
+});
+
+const IconStyle = styled(Box)`
+text-align: center;
+margin-top:20px;
+`;
+
+const FacebookStyle = styled(Facebook)`
+height: 50px;
+width: 50px;
+color:#3b5998;
+padding:10px;
+`;
+
+const InstagramStyle = styled(Instagram)`
+height: 50px;
+width: 50px;
+color:#d62976;
+padding:10px;
+`;
+
+const GitHubStyle = styled(GitHub)`
+height: 50px;
+width: 50px;
+color:#4078c0;
+padding:10px;
+`;
+
+const LinkedInStyle = styled(LinkedIn)`
+height: 50px;
+width: 50px;
+color:#0072b1;
+padding:10px;
+`;
+
+const CoPresentSharpStyle = styled(CoPresentSharp)`
+height: 50px;
+width: 50px;
+color:#3b5998;
+padding:10px;
+`;
 
 function About() {
   return (
-    <div>
-      
-    </div>
+    <ContainerMain container>
+
+      <Grid item lg={5} sm={10} xs={12}>
+        <Images src={Ravi} alt="me" />
+      </Grid>
+
+      <Grid item lg={6} sm={10} xs={12}>
+        <Header>About Us</Header>
+        <SubHeader>MERN Developer</SubHeader>
+        <IconStyle>
+          <Link to='https://www.facebook.com/profile.php?id=100007642129369'><FacebookStyle /></Link>
+
+          <Link to='https://www.instagram.com/ravi_singh_thakur_78/'><InstagramStyle /></Link>
+
+          <Link to='https://github.com/ravisingh007ravi'><GitHubStyle /></Link>
+
+          <Link to='https://www.linkedin.com/in/ravi-singh-763378182'><LinkedInStyle /></Link>
+
+          <Link to='https://comforting-bienenstitch-487890.netlify.app/'><CoPresentSharpStyle /></Link>
+        </IconStyle>
+        <BioData>Trained in Backend Development using Nodes with ReactJs, Express and MongoDB
+          Proficient in JavaScript. Hands-on in ES6
+          Understand caching fundamentals and hands-on with Redis
+          Hands-on experience with AWS S3 file upload and read Experienced in Agile methodology - Daily scrum, Sprint planning. Sprint review.
+          Participated in FunctionUp Coding competitions on HackerRank and LeetCode</BioData>
+        <HomeButton to='/' style={{ textDecoration: 'none', textAlign: 'center' }}>
+          <Button variant="outlined">HOME</Button>
+        </HomeButton>
+      </Grid>
+
+    </ContainerMain>
   )
 }
 
-export default About
+export default About 
--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,11 +1,92 @@
-import React from 'react'
+import { Box, styled } from '@mui/material';
+import { Button, Table, TableHead, TableRow, TableCell, TableBody, Grid } from '@mui/material';
+import HomeBanner from '../images/toy-bricks-table-with-word-blog.jpg';
+import { categories } from './HomeCategorieData';
+import { Link, useSearchParams } from 'react-router-dom';
+import { Post } from './AllComponents';
+
+
+
+const ImageBanner = styled('img')({
+    width: '100%',
+    height: '40vh',
+    objectFit: 'cover'
+});
+
+const Image = styled(Box)`
+    margin-top: 25px;
+   
+    width:100%;
+    height: 40vh;
+    object-fit: cover;
+    display: flex;
+    align-items: center;
+    justify-items: center;
+    flex-direction: column;
+`;
+
+const StyleTable = styled(Table)`
+    border: 1px solid rgba(224, 224, 224, 1);
+`;
+
+const Stylebutton = styled(Button)`
+    margin:20px;
+    width:85%;
+    background:#6495Ed;
+    color:#ffff;
+`;
+
+const StyleLink = styled(Link)({
+
+    textDecoration: 'none',
+    color: 'inherit'
+})
+
+
 
 function Banner() {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    const [serachParams] = useSearchParams();
+    const category = serachParams.get('category')
+
+
+
+    return (
+        <>
+            <Image>
+                <ImageBanner src={HomeBanner} alt="BannerImage" />
+            </Image>
+
+            <Grid container>
+                <Grid item lg={2} sm={2} xs={12}>
+
+                    <StyleLink to={`/create?category=${category || 'all'}`}>
+                        <Stylebutton variant="contained">Create Blog</Stylebutton>
+                    </StyleLink>
+                    <StyleTable>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell><StyleLink to='/'>All Categories</StyleLink></TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {categories.map(category => (
+                                <TableRow key={category.id}>
+                                    <TableCell>
+                                        <StyleLink to={`/?category=${category.type}`}> {category.type} </StyleLink>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </StyleTable>
+                </Grid>
+
+                <Grid container item xs={12} sm={10} xl={10}>
+                    <Post />
+                </Grid>
+            </Grid>
+        </>
+    )
 }
 
 export default Banner
